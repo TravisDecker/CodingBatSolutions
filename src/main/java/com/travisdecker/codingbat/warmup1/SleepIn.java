17678a3bf69b864d3445bc8be29c87273e179bf7
TravisDecker <decker4693@gmail.com>
2021-10-19 20:23:30 -0600

    Implementation of SleepIn, tests passing

--- src/main/java/com/travisdecker/codingbat/warmup1/SleepIn.java
+++ src/main/java/com/travisdecker/codingbat/warmup1/SleepIn.java
@@ -2,8 +2,18 @@ package com.travisdecker.codingbat.warmup1;
 
 public class SleepIn {
 
-  public boolean getSleepIn(boolean weekday, boolean vacation) {
+  /**
+   * The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are
+   * on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep
+   * in.
+   * <p>
+   * Example: sleepIn(false, false) → true sleepIn(true, false) → false sleepIn(false, true) → true
+   */
 
+  public boolean getSleepIn(boolean weekday, boolean vacation) {
+    if (!weekday || vacation) {
+      return true;
+    }
     return false;
   }
 }
