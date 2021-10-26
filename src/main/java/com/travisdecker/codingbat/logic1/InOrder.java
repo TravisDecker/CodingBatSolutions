121754c1a8d46fde29a8c6a5768a7a4db268b284
TravisDecker <decker4693@gmail.com>
2021-10-25 19:59:48 -0600

    impl InOrder

--- src/main/java/com/travisdecker/codingbat/logic1/InOrder.java
+++ src/main/java/com/travisdecker/codingbat/logic1/InOrder.java
@@ -14,7 +14,11 @@ public class InOrder {
    */
 
   public static boolean getInOrder(int a, int b, int c, boolean bOk) {
-
+    if (bOk && c > b) {
+      return true;
+    } else if (b > a && c > b) {
+      return true;
+    }
     return false;
   }
 }
